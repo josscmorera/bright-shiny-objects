06bf95f64d2457e12dccff258dbee5481e55c800
josscmorera <me@jossmorera.com>
2023-02-21 15:44:49 -0500

    zzzz

--- main.js
+++ main.js
@@ -2,14 +2,75 @@
 // │ Your code here! │
 // └─────────────────┘
 
-
-
-
-
-
-
-
-
+function createUser(firstName, lastName) {
+    let user = {
+      firstName: firstName,
+      lastName: lastName
+    };
+    return user;
+  }  
+  
+  function setAge(user, age) {
+    user.age = age;
+    return user;
+  }
+  
+  function incrementAge(user) {
+    user.age++;
+    return user;
+  }
+  
+  function fixCar(car) {
+    car.needsMaintenance = false;
+    return car;
+  }
+  
+  function addGrades(student, grades) {
+    for (let i = 0; i < grades.length; i++) {
+      student.grades.push(grades[i]);
+    }
+      return student;
+  }
+  
+  function getDataType(obj, key) {
+    return typeof obj[key];
+  }
+  
+  function addTodo(todos, todo) {
+    todos.push(todo);
+    return todos;
+  }
+  
+  function addSong(playlist, song) {
+    playlist.duration += song.duration;
+    playlist.songs.push(song);
+    return playlist;
+  }
+  
+  function updateReportCard(reportCard, newGrade) {
+    reportCard.grades.push(newGrade);
+    let lowestGrade = reportCard.grades[0];
+    for (let i = 1; i < reportCard.grades.length; i++) {
+      if (reportCard.grades[i] < lowestGrade) {
+        lowestGrade = reportCard.grades[i];
+      }
+    }
+    reportCard.lowestGrade = lowestGrade;
+    let highestGrade = reportCard.grades[0];
+    for (let i = 1; i < reportCard.grades.length; i++) {
+      if (reportCard.grades[i] > highestGrade) {
+        highestGrade = reportCard.grades[i];
+      }
+    }
+    reportCard.highestGrade = highestGrade;
+    let sum = 0;
+    for (let i = 0; i < reportCard.grades.length; i++) {
+      sum += reportCard.grades[i];
+    }
+    reportCard.averageGrade = sum / reportCard.grades.length;
+    return reportCard;
+  }
+  
 
 // ┌─────────────────────────────────────────────────────────────────────────┐
 // │ Code used for testing. Do not modify!                                   │                         
